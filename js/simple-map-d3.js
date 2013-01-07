f9e14229ca37ada506804c6fea7450cb42d0aff8
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2013-01-07 12:31:29 -0600

    Adding second example and ability to have more than one map.

--- js/simple-map-d3.js
+++ js/simple-map-d3.js
@@ -3,8 +3,9 @@
  *
  * Colors from http://colorbrewer2.org/
  */
-var SimpleMapD3 = (function() {
-  // Private variables and functions
+function SimpleMapD3(o) {
+  var smd = {};
+  
   var defaults = {
     stroke: '#898989',
     fill: '#FFFFFF',
@@ -22,163 +23,155 @@ var SimpleMapD3 = (function() {
   };
   
   // Constructor
-  var SimpleMapD3 = function(options) {
+  smd.constructor = function(options) {
     // Extend defaults
     var extended = defaults;
     for (var prop in options) {
       extended[prop] = options[prop];
     }
-    this.options = extended;
-    
+    smd.options = extended;
+
     // Check if data was given, or if data source was given
-    if (this.options.data === Object(this.options.data)) {
-      this.data = this.options.data;
-      this.dataLoaded(this.data);
+    if (smd.options.data === Object(smd.options.data)) {
+      smd.data = smd.options.data;
+      smd.dataLoaded(smd.data);
     }
-    else if (toString.call(this.options.datasource) == '[object String]') {
-      this.getData();
+    else if (toString.call(smd.options.datasource) == '[object String]') {
+      smd.getData();
     }
   };
   
-  // Prototype "public" methods
-  SimpleMapD3.prototype.constructor = SimpleMapD3,
-  
   // Get data
-  SimpleMapD3.prototype.getData = function() {
-    var thisMap = this;
-    
-    d3.json(this.options.datasource, function(data) {
-      thisMap.data = data;
-      thisMap.loadData();
+  smd.getData = function() {
+    d3.json(smd.options.datasource, function(data) {
+      smd.data = data;
+      smd.loadData();
     });
   };
   
   // Handle data once loaded
-  SimpleMapD3.prototype.loadData = function(data) {
-    if (this.data === void 0) {
-      this.data = data;
+  smd.loadData = function(data) {
+    if (smd.data === void 0) {
+      smd.data = data;
     }
-    this.createCanvas();
-    this.project();
-    this.render();
+    smd.canvas();
+    smd.project();
+    smd.render();
   };
   
   // Load up canvas, set size to container
-  SimpleMapD3.prototype.createCanvas = function() {
-    this.container = d3.select(this.options.container);
-    this.width = parseFloat(this.container.style('width'));
-    this.height = parseFloat(this.container.style('height'));
-    this.canvas = this.container.append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height);
+  smd.canvas = function() {
+    smd.container = d3.select(smd.options.container);
+    smd.width = parseFloat(smd.container.style('width'));
+    smd.height = parseFloat(smd.container.style('height'));
+    smd.canvas = smd.container.append('svg')
+      .attr('width', smd.width)
+      .attr('height', smd.height);
       
     // Add tooltip
-    if (this.options.tooltipOn === true) {
-      this.container.classed('simple-map-d3-tooltip-container', true);
-      this.container.append('div').classed('simple-map-d3-tooltip', true);
+    if (smd.options.tooltipOn === true) {
+      smd.container.classed('simple-map-d3-tooltip-container', true);
+      smd.container.append('div').classed('simple-map-d3-tooltip', true);
     }
   };
   
   // Project data into the canvas
-  SimpleMapD3.prototype.project = function() {
-    this.proj = d3.geo.mercator().scale(1).translate([0,0]);
-    this.projOptions = this.projOptions || {};
+  smd.project = function() {
+    smd.proj = d3.geo.mercator().scale(1).translate([0,0]);
+    smd.projOptions = smd.projOptions || {};
     
     // Variables
-    var margin = this.width * 0.02;
-    var bounds0 = d3.geo.bounds(this.data);
-    var bounds = bounds0.map(this.proj);
-    var xscale = (this.width - 2 * margin) / Math.abs(bounds[1][0] - bounds[0][0]);
-    var yscale = (this.height - 2 * margin) / Math.abs(bounds[1][1] - bounds[0][1]);
+    var margin = smd.width * 0.02;
+    var bounds0 = d3.geo.bounds(smd.data);
+    var bounds = bounds0.map(smd.proj);
+    var xscale = (smd.width - 2 * margin) / Math.abs(bounds[1][0] - bounds[0][0]);
+    var yscale = (smd.height - 2 * margin) / Math.abs(bounds[1][1] - bounds[0][1]);
     var pscale = Math.min(xscale, yscale);
     var wscale = pscale;
     var d, widthd, heightd;
     
     // Handle projection
-    this.proj.scale(pscale);
-    this.proj.translate(this.proj([-bounds0[0][0], -bounds0[1][1]]));
-    this.projOptions.path = d3.geo.path().projection(this.proj);
+    smd.proj.scale(pscale);
+    smd.proj.translate(smd.proj([-bounds0[0][0], -bounds0[1][1]]));
+    smd.projOptions.path = d3.geo.path().projection(smd.proj);
     
     // Handle svg canvas, dpeneding on orientation
     if (xscale > yscale) {
       d = xscale * Math.abs(bounds[1][0] - bounds[0][0]) - yscale * Math.abs(bounds[1][0] - bounds[0][0]);
-      this.canvas.attr('transform', 'translate(' + d / 2 + ', 0)');
+      smd.canvas.attr('transform', 'translate(' + d / 2 + ', 0)');
     }
     else {
       d = yscale * Math.abs(bounds[1][1] - bounds[0][1]) - xscale * Math.abs(bounds[1][1] - bounds[0][1]);
-      this.canvas.attr('transform', 'translate(0, ' + d / 5 + ')');
+      smd.canvas.attr('transform', 'translate(0, ' + d / 5 + ')');
     }
     
     // Handle offset, depending on orientation
-    widthd = this.proj(bounds0[0])[1];
-    heightd = this.proj(bounds0[1])[0];
+    widthd = smd.proj(bounds0[0])[1];
+    heightd = smd.proj(bounds0[1])[0];
     if (xscale > yscale) {
-      this.projOptions.offsetxd = (this.width / 2 - widthd / 2);
-      this.projOptions.offsetyd = margin;
+      smd.projOptions.offsetxd = (smd.width / 2 - widthd / 2);
+      smd.projOptions.offsetyd = margin;
     }
     else {
-      this.projOptions.offsetxd = margin;
-      this.projOptions.offsetyd = (this.height / 2 - heightd / 2);
+      smd.projOptions.offsetxd = margin;
+      smd.projOptions.offsetyd = (smd.height / 2 - heightd / 2);
     }
   };
   
   // Make color range
-  SimpleMapD3.prototype.makeColorRange = function() {
-    var thisMap = this;
-    
-    var min = d3.min(this.data.features, function(d) { return d.properties[thisMap.options.colorProperty]; });
-    var max = d3.max(this.data.features, function(d) { return d.properties[thisMap.options.colorProperty]; });
+  smd.makeColorRange = function() {
+    var min = d3.min(smd.data.features, function(d) { return d.properties[smd.options.colorProperty]; });
+    var max = d3.max(smd.data.features, function(d) { return d.properties[smd.options.colorProperty]; });
     // Use a sort of sensible, proportional color step
-    this.options.colorStep = this.options.colorStep || ((max - min) / this.options.colorSet.length / 2);
+    smd.options.colorStep = smd.options.colorStep || ((max - min) / smd.options.colorSet.length / 2);
 
-    this.colorRange = d3.scale.linear()
-      .domain(d3.range(min, max, this.options.colorStep))
-      .range(this.options.colorSet)
+    smd.colorRange = d3.scale.linear()
+      .domain(d3.range(min, max, smd.options.colorStep))
+      .range(smd.options.colorSet)
       .clamp(true);
   };
   
   // Callback for attribute: Fill
-  SimpleMapD3.prototype.attributeFill = function(d) {
-    if (this.options.colorOn === false) {
-      return this.options.fill;
+  smd.attributeFill = function(d) {
+    if (smd.options.colorOn === false) {
+      return smd.options.fill;
     }
     else {
-      if (!this.ColorRange) {
-        this.makeColorRange();
+      if (!smd.ColorRange) {
+        smd.makeColorRange();
       }
-      return this.colorRange(d.properties[this.options.colorProperty]);
+      return smd.colorRange(d.properties[smd.options.colorProperty]);
     }
   };
   
   // Render
-  SimpleMapD3.prototype.render = function() {
-    var thisMap = this;
-  
-    this.canvas
+  smd.render = function() {
+    smd.canvas
       .selectAll('path')
-        .data(this.data.features)
+        .data(smd.data.features)
       .enter().append('path')
-        .attr('d', this.projOptions.path)
-        .attr('stroke', this.options.stroke)
-        .attr('fill', function(d) { return thisMap.attributeFill(d); })
-        .attr('transform', 'translate(' + this.projOptions.offsetxd + ', ' + this.projOptions.offsetyd + ')')
+        .attr('d', smd.projOptions.path)
+        .attr('stroke', smd.options.stroke)
+        .attr('fill', function(d) { return smd.attributeFill(d); })
+        .attr('transform', 'translate(' + smd.projOptions.offsetxd + ', ' + smd.projOptions.offsetyd + ')')
         .on('mouseover', function(d) {
           // Tooltip
-          if (thisMap.options.tooltipOn === true) {
-            thisMap.container.select('.simple-map-d3-tooltip')
+          if (smd.options.tooltipOn === true) {
+            smd.container.select('.simple-map-d3-tooltip')
               .style('display', 'block')
-              .html(thisMap.options.tooltipContent(d));
+              .html(smd.options.tooltipContent(d));
           }
         })
         .on('mouseout', function(d) {
           // Tooltip
-          if (thisMap.options.tooltipOn === true) {
-            thisMap.container.select('.simple-map-d3-tooltip')
+          if (smd.options.tooltipOn === true) {
+            smd.container.select('.simple-map-d3-tooltip')
               .style('display', 'none');
           }
         });
   };
   
-  // return module
-  return SimpleMapD3;
-})();
+  // Construct and return our map object.
+  smd.constructor(o);
+  return smd;
+};
